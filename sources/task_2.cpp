f782a80d092ea22b7471dffe9da0e1efde214967
Reznw <48454801+Reznw@users.noreply.github.com>
2021-10-18 17:46:40 +0300

    Update task_2.cpp

--- sources/task_2.cpp
+++ sources/task_2.cpp
@@ -1,34 +1,54 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2021 Reznw
 
 #include <tasks.hpp>
 
 // Task 2.
 int sum(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    size_t sumall = 0;
+    for (size_t i = 0; i < input.size(); ++i) {
+        sumall += input[i];
+    }
+    return sumall;
 }
 
 int square_sum(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    size_t sumall = 0;
+    for (size_t i = 0; i < input.size(); ++i) {
+        sumall += (input[i] * input[i]);
+    }
+    return sumall;
 }
 
 int sum_six(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    size_t sumall = 0;
+    for (size_t i = 0; i < 6; ++i) {
+        sumall += input[i];
+    }
+    return sumall;
 }
 
-int sum_k(const std::vector<int>& input, int k1, int k2) {
-    // Fix it.
-    return 0;
+int sum_k(const std::vector<int>& input, size_t k1, size_t k2) {
+    size_t sumall = 0;
+    for (size_t i = k1; i <= k2; ++i) {
+        sumall += input[i];
+    }
+    return sumall;
 }
 
 int mean(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    size_t sumall = 0;
+    for (size_t i = 0; i < input.size(); ++i) {
+        sumall += input[i];
+    }
+    size_t average = sumall / input.size();
+    return average;
 }
 
-int mean_k(const std::vector<int>& input, int s1, int s2) {
-    // Fix it.
-    return 0;
+int mean_k(const std::vector<int>& input, size_t s1, size_t s2) {
+    size_t sumall = 0;
+    for (size_t i = s1; i <= s2; ++i) {
+        sumall += input[i];
+    }
+    size_t average = sumall / (s2 -s1 + 1);
+    return average;
 }
