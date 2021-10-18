29e790804ac1f20f10c6910296dbc99b84153f35
Reznw <48454801+Reznw@users.noreply.github.com>
2021-10-18 17:48:18 +0300

    Update task_4.cpp

--- sources/task_4.cpp
+++ sources/task_4.cpp
@@ -1,12 +1,22 @@
-// Copyright 2018 Your Name <your_email>
-
+// Copyright 2021 Reznw
 #include <tasks.hpp>
 
 // Task 4.
 void remove_negative(std::vector<int>& input) {
-    // Fix it.
+    for (size_t i = 0; i < input.size(); ++i) {
+        if (input[i] < 0) {
+            input.erase(input.begin() + i);
+            break;
+        }
+    }
 }
 
 void remove_last_even(std::vector<int>& input) {
-    // Fix it.
+    size_t even_number;
+    for (size_t i = 0; i < input.size(); ++i) {
+        if (input[i] % 2 == 0) {
+            even_number = i;
+        }
+    }
+    input.erase(input.begin() + even_number);
 }
